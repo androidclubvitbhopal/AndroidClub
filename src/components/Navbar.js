19ba6ecd20a596e1b06c94ab4a7f13bda93798e2
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-03-01 17:53:52 +0400

    1.0

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+function Navbar(){
+    return(
+        <div className="Navbar">
+            <Link to="/home">Home</Link>
+        </div>
+    )
+}
+export default Navbar
