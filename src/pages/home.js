8506d4cb9803a035af753626cd92948b8b815e1a
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-03-05 16:22:28 +0400

    1.0

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,23 +2,24 @@ import Navbar from "../components/Navbar"
 import { useContext, useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseconfig";
-import { collection, query, where } from "firebase/firestore";
+import { collection, query, updateDoc, where } from "firebase/firestore";
 import { db } from "../firebaseconfig";
 import { getDocs,doc } from "firebase/firestore";
-import PopUpWindow from "../components/PopUpRegistration";
 import { Link } from "react-router-dom";
 import { Authcontext } from "../contextProvider";
 
 function Home(){
     const [Event,setEvent] = useState([])
-    const [Ev,setEv] = useState([])
+    const [Ev,setEv] = useState({})
     const [vis,setVis] = useState("hidden")
     const {currentUser} = useContext(Authcontext)
+    const [UserDetails,setDetails] = useState({})
     
 
-    const usersRef = collection(db, "events");
+    const eventsRef = collection(db, "events");
+    const usersRef = collection(db, "users");
     const FetchEvents = async ()=> {
-        const q = query(usersRef)
+        const q = query(eventsRef)
         const temp = []
         const querySnapShot = await getDocs(q)
         try{
@@ -30,25 +31,35 @@ function Home(){
             console.log(err)
         }
     }
+    const FetchUserDetails =async()=>{
+        const q = query(usersRef,where('email','==',currentUser.email))
+        const temp = []
+        const querySnapShot = await getDocs(q)
+        try{
+            querySnapShot.forEach((doc)=>{
+                temp.push(doc.data())
+            })
+            setDetails({email:temp[0].email,name:`${temp[0].name}`,paymentVerified:"N",phone:`${temp[0].phone}`,regNo:`${temp[0].regNo}`})
+        }catch(err){
+            console.log(err)
+        }
+    }
     useEffect(()=>{
         FetchEvents()
+        FetchUserDetails()
     },[])
-    useEffect(()=>{
-        console.log(currentUser)
-    },[currentUser])
-    const HandleSubmit=(e)=>{
-        e.preventDefault()
-        const name = e.target[0].value
-        const email = e.target[1].value
-        const number = e.target[2].value
-        const RgNo = e.target[3].value
-        const gPay = e.target[4].files[0]
 
-        setVis("hidden")
-    }
+
+    // for testing use only 
+
+    // useEffect(()=>{
+    //     console.log(Ev)
+    // },[Ev])
+
+
     const HandleRegister= async (EventName)=>{
         setVis("visible")
-        const q = query(usersRef,where("notificationGroup", "==", `${EventName}`))
+        const q = query(eventsRef,where("notificationGroup", "==", `${EventName}`))
         const querySnapShot = await getDocs(q)
         const temp = []
         try{
@@ -56,6 +67,19 @@ function Home(){
                 temp.push(doc.data())
             })
             setEv(temp)
+            let RegEmails = temp[0]["Registered Emails"]
+            RegEmails = [...RegEmails,`${currentUser.email}`]
+            let RegInfo = temp[0]["Registered Users"]
+            RegInfo = [...RegInfo,UserDetails]
+            
+            // await updateDoc(doc(db,"users",currentUser.uid),{
+            //     allRegisteredEvents:[...allRegisteredEvents,Ev]
+            // })
+
+            await updateDoc(doc(db,"events",Ev[0].notificationGroup),{
+                "Registered Emails":RegEmails,
+                "Registered Users":RegInfo,
+            })
         }catch(err){
             console.log(err)
         }
@@ -63,16 +87,17 @@ function Home(){
     return(
         <div className="Home">
             {/* <PopUpWindow style={{visibility:`${vis}`}}/> */}
-            <div className="PopUpWindow" onClick={()=>{setVis("hidden")}} style={{visibility:`${vis}`}}>
-                <input className="CancelBtn" type='button' onClick={()=>{setVis("hidden")}} value='X'></input>
-                {!currentUser && 
-                    <div className="PopUpForm">
-                        <p>Login In or Register to Access all features.</p>
-                        <Link to='login'>Login</Link>
-                        <Link to='Register'>Register</Link>
-                    </div>
-                }
-            </div>
+            {
+                !currentUser &&
+                <div className="PopUpWindow" onClick={()=>{setVis("hidden")}} style={{visibility:`${vis}`}}>
+                    <input className="CancelBtn" type='button' onClick={()=>{setVis("hidden")}} value='X'></input>
+                        <div className="PopUpForm">
+                            <p>Login In or Register to Access all features.</p>
+                            <Link to='login'>Login</Link>
+                            <Link to='Register'>Register</Link>
+                        </div>
+                </div>
+            }
 
             <Navbar/>
             <div className="Events">
