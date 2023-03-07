3ccbefb16555750bb3cd60e0c8f0e457fffc728a
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-03-07 17:21:55 +0400

    1.0

---
+++ src/pages/EventStream.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+function Stream(props){
+    const [vis,setVis] = useState(`${props.vis}`)
+    const Vis =()=>{
+        setVis("hidden")
+    }
+    return(
+        <div className="StreamPage">
+            <iframe id="ytplayer" type="text/html" 
+            src={`https://www.youtube.com/embed/qVUv8PCRHCc?autoplay=1&origin=http://example.com`}
+            frameborder="0"></iframe>
+            <iframe width="185" height="315" src="https://www.youtube.com/live_chat?v=qVUv8PCRHCc&embed_domain=localhost:3001/LiveStream" frameborder="0"></iframe>
+            <button>Close</button>
+        </div>
+    )
+}
+export default Stream
