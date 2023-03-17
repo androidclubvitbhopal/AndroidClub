65644fc0a67b6ee911e061890f830ab22a04ba75
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2023-03-17 13:27:23 +0400

    1.0

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,171 +5,192 @@ import { auth } from "../../firebaseconfig";
 import { collection, query, updateDoc, where } from "firebase/firestore";
 import { db } from "../../firebaseconfig";
 import { getDocs, doc } from "firebase/firestore";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import { Authcontext } from "../../contextProvider";
 import homeBg from "../../images/home-bg.png"
 import { Footer } from "../../components/Footer/Footer";
 import "./home.css";
 
-function Home() {
-    const [Event, setEvent] = useState([])
-    const [Ev, setEv] = useState({})
-    const [vis, setVis] = useState("hidden")
-    const { currentUser } = useContext(Authcontext)
-    const [UserDetails, setDetails] = useState({})
-    const [userEvents, setUserEvents] = useState([])
-    // const [RgSt,setSt] = useState(false)
+function Home(){
+    const navigate = useNavigate()
+    const [UEvent,setUEvent] = useState([])
+    const [CEvent,setCEvent] = useState([])
+    const [vis,setVis] = useState("hidden")
+    const {currentUser} = useContext(Authcontext)
+    const [UserDetails,setDetails] = useState({})
+    const [userEvents,setUserEvents]  =useState([])
+    const {Evpayment,setEvPay} = useContext(Authcontext)
+    let j=false;
+    let k=0;
 
     const eventsRef = collection(db, "events");
     const usersRef = collection(db, "users");
 
 
-    const FetchEvents = async () => {
-        const q = query(eventsRef)
-        const temp = []
-        const querySnapShot = await getDocs(q)
-        try {
-            querySnapShot.forEach((doc) => {
-                temp.push(doc.data())
+    const FetchEvents = async ()=> {
+        const q1 = query(eventsRef,where("completion","==",false))
+        const temp1 = []
+        const querySnapShot1 = await getDocs(q1)
+        try{
+            querySnapShot1.forEach((doc)=>{
+                temp1.push(doc.data())
             })
-            setEvent(temp)
-        } catch (err) {
+            setUEvent(temp1)
+        }catch(err){
+            console.log(err)
+        }
+        const q2 = query(eventsRef,where("completion","==",true))
+        const temp2 = []
+        const querySnapShot2 = await getDocs(q2)
+        try{
+            querySnapShot2.forEach((doc)=>{
+                temp2.push(doc.data())
+            })
+            setCEvent(temp2)
+        }catch(err){
             console.log(err)
         }
     }
 
 
-    const FetchUserDetails = async () => {
-        const q = query(usersRef, where('email', '==', currentUser.email))
+    const FetchUserDetails =async()=>{
+        const q = query(usersRef,where('email','==',currentUser.email))
         const temp = []
         const querySnapShot = await getDocs(q)
-        try {
-            querySnapShot.forEach((doc) => {
+        try{
+            querySnapShot.forEach((doc)=>{
                 temp.push(doc.data())
             })
-            setDetails({ email: temp[0].email, name: `${temp[0].name}`, paymentVerified: "N", phone: `${temp[0].phone}`, regNo: `${temp[0].regNo}` })
+            setDetails({email:temp[0].email,name:`${temp[0].name}`,paymentVerified:"N",phone:`${temp[0].phone}`,regNo:`${temp[0].regNo}`,paymentImgURL:"",location:""})
             setUserEvents(temp[0].allRegisteredEvents)
-        } catch (err) {
+        }catch(err){
             console.log(err)
         }
     }
-    useEffect(() => {
+    useEffect(()=>{
         FetchEvents()
         FetchUserDetails()
-    }, [])
+        
+    },[])
 
 
     // for testing use only 
 
-    useEffect(() => {
-        console.log(Ev)
-    }, [Ev])
+    // useEffect(()=>{
+    //     console.log(Ev)
+    // },[Ev])
 
-
-    const HandleRegister = async (EventName) => {
+    useEffect(()=>{
+        j=true;
+    },[UEvent])
+    useEffect(()=>{
+        k=k+1;
+    },[UserDetails])
+    
+    const HandleInit=(Event)=>{
+        setEvPay(Event)
+        navigate("/Payments")
+    }
+    const HandleRegister= async (EventName)=>{
         setVis("visible")
-        const q = query(eventsRef, where("notificationGroup", "==", `${EventName}`))
+        const q = query(eventsRef,where("notificationGroup", "==", `${EventName}`))
         const querySnapShot = await getDocs(q)
         const temp = []
-        try {
-            querySnapShot.forEach((doc) => {
-                temp.push(doc.data())
-            })
-            setEv(temp)
-            console.log(temp)
-            let RegEmails = temp[0]["Registered Emails"]
-            RegEmails = [...RegEmails, `${currentUser.email}`]
-            let RegInfo = temp[0]["Registered Users"]
-            RegInfo = [...RegInfo, UserDetails]
-
-            let UserEvents = userEvents
-            UserEvents = [...UserEvents, temp[0]]
-            await updateDoc(doc(db, "users", currentUser.uid), {
-                allRegisteredEvents: UserEvents
-            })
-
-            await updateDoc(doc(db, "events", Ev[0].notificationGroup), {
-                "Registered Emails": RegEmails,
-                "Registered Users": RegInfo,
-            })
-        } catch (err) {
+        try{
+                querySnapShot.forEach((doc)=>{
+                    temp.push(doc.data())
+                })
+                // setEv(temp)
+                let RegEmails = temp[0]["Registered Emails"]
+                RegEmails = [...RegEmails,`${currentUser.email}`]
+                if(k!=0){
+                    let RegInfo = temp[0]["Registered Users"]
+                    RegInfo = [...RegInfo,UserDetails]
+                    console.log(UserDetails)
+                    let UserEvents = userEvents
+                    UserEvents = [...UserEvents,{name:temp[0].name,description:temp[0].description,time:temp[0].time,bannerURL:temp[0].bannerURL,location:temp[0].location}]
+                    await updateDoc(doc(db,"events",EventName),{
+                        "Registered Emails":RegEmails,
+                        "Registered Users":RegInfo,
+                    }).then(async()=>{
+                        await updateDoc(doc(db,"users",currentUser.uid),{
+                            allRegisteredEvents:UserEvents
+                        })
+                    })
+                    k=0;
+            }
+        }catch(err){
             console.log(err)
         }
+        navigate("/RegisteredEvents")
+        alert(`Registered for ${EventName}`)
     }
-    return (
+    return(
         <div className="Home">
-            {/* <PopUpWindow style={{visibility:`${vis}`}}/> */}
             {
                 !currentUser &&
-                <div className="PopUpWindow" onClick={() => { setVis("hidden") }} style={{ visibility: `${vis}` }}>
-                    <div className="PopUpForm">
-                        <p>Hey Learner!! <br></br>Login In or Register to Access all features.</p>
-                        <Link id='New' to='../login'>Login</Link>
-                        <Link id='New' to='../Register'>Register</Link>
-                        <input className="CancelBtn" type='button' onClick={() => { setVis("hidden") }} value='Close'></input>
-                    </div>
+                <div className="PopUpWindow" onClick={()=>{setVis("hidden")}} style={{visibility:`${vis}`}}>
+                        <div className="PopUpForm">
+                            <p>Hey Learner!! <br></br>Login In or Register to Access all features.</p>
+                            <Link id='New' to='login'>Login</Link>
+                            <Link id='New' to='Register'>Register</Link>
+                            <input className="CancelBtn" type='button' onClick={()=>{setVis("hidden")}} value='Close'></input>
+                        </div>
                 </div>
             }
+            <Navbar/>
+            <p className='Heading1'>All Upcoming Events</p>
+            <div className="Events">
+                {
+                    UEvent.map((Events)=>{
+                        if(j){
+                            return(
+                                <div className="Event" style={{backgroundImage:`url(${Events.bannerURL})`}} onClick={()=>{setVis("visible")}}>
+                                    <div className="moreInfo">
+                                        <div className="EventName">{Events.name}</div>
+                                        <p className="mode"><b>Location:  </b>{Events.location}</p>
+                                        <p className="description">{Events.description}</p>
+                                        <p className="time"><b>Time:  </b>{Events.time}</p>
+                                        <p className="Price"><b>Price:  ₹</b>{Events.price}</p>
+                                    </div>
+                                    {
+                                        currentUser && Events.price==0 &&
+                                        <button className="RegisterBtn" onClick={()=>{HandleRegister(Events.notificationGroup)}}><span type='text'>Register</span></button>
+                                    }
+                                    {
+                                        currentUser && Events.price!=0 &&
+                                        <button className="RegisterBtn" onClick={()=>{HandleInit(Events)}}><span type='text'>Register</span></button>
 
-            <Navbar />
-            <div className="club-intro">
-            <h1 className="first-heading">Android club VIT Bhopal </h1> <br />
-                <p>We at Android Club are driven to achieve excellence and solve problems while at it. Dedicated to educating and creating awareness about modern Mobile App development, we host workshops, hackathons, webinars, and all possible events under the sun, that help us build an inclusive community of like-minded people who explore and learn together. So, wear your thinking caps, put on some creativity, and let's develop some amazing apps!</p>
-                <div className="home-bg-div">
-                </div>
-            </div>
-            <div className="upcoming-events">
-                <p className='upcoming-events-heading'>All Upcoming Events</p>
-                <div className="upcoming-events-container">
-                    {
-                        Event.map((Events) => {
-                            if (Events.completion === false) {
-                                return (
-                                    <div key={Math.random()} className="upcoming-event-block" onClick={() => { setVis("visible") }} style={{ backgroundImage: `url(${Events.bannerURL})` }}>
-                                        <div className="upcoming-event-info">
-                                            <div className="upcoming-event-name">{Events.name}</div> <br />
-                                            <p className="upcoming-event-mode" >Mode : {Events.location}</p>
-                                            <p className="upcoming-event-description">Details : {Events.description}</p>
-                                            <p className="upcoming-event-time">Time : {Events.time}</p>
-                                            <p className="upcomin-event-price">Price : ₹{Events.price}</p>
-                                        </div>
-                                        {
-                                            currentUser &&
-                                            <button className="RegisterBtn" onClick={() => { HandleRegister(Events.notificationGroup) }}>Register</button>
-                                        }
+                                    }
+                                </div>
+                            )
+                        }
+                        else if(!j){
+                            return(
+                                <div className="Event" onClick={()=>{setVis("visible")}} style={{background:'green'}}>
+                                    <div className="NoInfo">
+                                        <p className="EventName">No Events Scheduled as of Now <br></br>Come back later!!</p>
                                     </div>
-                                )
-                            }
-                        })
-                    }
-                </div>
+                                </div>
+                            )
+                        }
+                    })
+                }
             </div>
-            <div className="completed-events">
-                <p className='completed-events-heading'>Completed Events</p>
-                <div className="completed-events-container">
-                    {
-                        Event.map((Events) => {
-                            if (Events.completion === true) {
-                                return (
-                                    <div key={Math.random()} className="completed-event-block" onClick={() => { setVis("visible") }}>
-                                        <div className="completed-event-info">
-                                            <img src={Events.bannerURL} className="completed-event-img" alt="" /> 
-                                            <div className="completed-events-details">
-                                            <div className="completed-event-name">{Events.name}</div>
-                                            <p className="completed-event-description">{Events.description}</p>
-                                            <p className="completed-event-summary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque perferendis sapiente vero veniam et hic voluptatibus quis quam voluptas blanditiis nulla, sequi ipsam a, enim voluptate, incidunt reiciendis dignissimos eius veritatis officiis sint repudiandae cum possimus. Ratione praesentium debitis similique laudantium ut vel, iure explicabo id, voluptas commodi eligendi dolorem.</p>
-                                            <p className="completed-event-date">{Events.date}</p>
-                                            <p className="completed-event-location"> Venue: {Events.location}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        })
-                    }
-                </div>
+            <p className='Heading2'>Completed Events</p>
+            <h3> Whether you're a seasoned pro or just looking to try something new, you're sure to find a community of like-minded individuals ready to welcome you with open arms. Check out our completed events section to see what our club has been up to, and join in on the fun!</h3>
+            <div className="Events" style={{marginTop:'15%'}}>
+                {
+                    CEvent.map((Events)=>(
+                        <div className="Event" onClick={()=>{setVis("visible")}} style={{backgroundImage:`url(${Events.bannerURL})`}}>
+                            <div className="moreInfo">
+                            <div className="EventName">{Events.name}</div>
+                                <p className="description">{Events.description}</p>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
-            <Footer/>
         </div>
     )
 }
