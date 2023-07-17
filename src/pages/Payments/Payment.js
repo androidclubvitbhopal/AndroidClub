d3d20fc31b067f48039cb7b6ca89852cb657667f
Akshay Mathilakth Nair <108605741+akshaynair5@users.noreply.github.com>
2023-07-17 11:43:50 +0530

    Update Payment.js

--- src/pages/Payments/Payment.js
+++ src/pages/Payments/Payment.js
@@ -16,8 +16,11 @@ function Payment(){
     const {Evpayment,setEvPay} = useContext(Authcontext)
     const eventsRef = collection(db, "events");
     const usersRef = collection(db, "users");
-    const [UserDetails,setDetails] = useState({})
+    const [UserDetails,setUDetails] = useState({})
     const [userEvents,setUserEvents]  =useState([])
+
+    const [details,setDetails] = useState([])
+    const [teamName,setTN] = useState("")
     const [err,setErr] = useState(false)
 
     const FetchUserDetails =async()=>{
@@ -28,7 +31,7 @@ function Payment(){
             querySnapShot.forEach((doc)=>{
                 temp.push(doc.data())
             })
-            setDetails({email:temp[0].email,name:`${temp[0].name}`,paymentVerified:"N",phone:`${temp[0].phone}`,regNo:`${temp[0].regNo}`,paymentImgURL:"",location:""})
+            setUDetails({email:temp[0].email,name:`${temp[0].name}`,paymentVerified:"N",phone:`${temp[0].phone}`,regNo:`${temp[0].regNo}`,paymentImgURL:"",location:""})
             setUserEvents(temp[0].allRegisteredEvents)
         }catch(err){
             console.log(err)
@@ -38,8 +41,36 @@ function Payment(){
         FetchUserDetails()
     },[])
     useEffect(()=>{
-        console.log(Evpayment)
+        console.log(Evpayment.noOfmembers)
     })
+
+    const HandleDetailsSubmit =async(e) =>{
+        e.preventDefault();
+        let teamInfo = []
+
+        for(let i=3;i<=12;i=i+2){
+            if(i==3){
+                const name = e.target[2].value
+                const phoneNo = e.target[3].value
+                const memR = e.target[4].value
+                const memEmail = e.target[5].value
+                const temp = {name:`${name}`,phoneNo:phoneNo,registrationNo:`${memR}`,email:`${memEmail}`}
+                teamInfo = [...teamInfo,temp];
+                i=5;
+
+            }
+            else{
+                const memR = e.target[i-1].value
+                const memEmail = e.target[i].value
+                const temp = {registrationNo:`${memR}`,email:`${memEmail}`}
+                teamInfo = [...teamInfo,temp];
+            }
+        }
+        console.log(teamInfo);
+        setDetails(teamInfo)
+        setTN({teamName:e.target[0].value,Culture:e.target[1].value});
+
+    }
     const  HandleSubmit=async(e)=>{
         let k = false;
         e.preventDefault()
@@ -48,7 +79,7 @@ function Payment(){
         const temp = []
         const PaymentSS = e.target[0].files[0]
         const storageid = new Date().getTime()
-        const storageRef = ref(storage,`${storageid}`)
+        const storageRef = ref(storage,`EventPayments/${Evpayment.notificationGroup}/${currentUser.email}`)
         querySnapShot.forEach((doc)=>{
             temp.push(doc.data())
         })
@@ -63,51 +94,161 @@ function Payment(){
             navigate("/RegisteredEvents")
         }
         else{
-            RegEmails = [...RegEmails,`${currentUser.email}`]
-            let RegInfo = temp[0]["Registered Users"]
-            let UserEvents = userEvents
-            UserEvents = [...UserEvents,`${temp[0].notificationGroup}`]
-            await uploadBytesResumable(storageRef,PaymentSS)
-                .then(()=>{
-                    getDownloadURL(storageRef).then(async (downloadURL) => {
-                        try{
-                            console.log('heyyy')
-                            let UD = UserDetails
-                            UD.paymentImgURL = `${downloadURL}`
-                            RegInfo = [...RegInfo,UD]
-                            await updateDoc(doc(db,"events",Evpayment.notificationGroup),{  
-                                "Registered Emails":RegEmails,
-                                "Registered Users":RegInfo,
-                            }).then(async()=>{
-                                await updateDoc(doc(db,"users",currentUser.uid),{
-                                    allRegisteredEvents:UserEvents
+            if(Evpayment.noOfmembers==1){
+                RegEmails = [...RegEmails,`${currentUser.email}`]
+                let RegInfo = temp[0]["Registered Users"]
+                let UserEvents = userEvents
+                UserEvents = [...UserEvents,`${temp[0].notificationGroup}`]
+                await uploadBytesResumable(storageRef,PaymentSS)
+                    .then(()=>{
+                        getDownloadURL(storageRef).then(async (downloadURL) => {
+                            try{
+                                console.log('heyyy')
+                                let UD = UserDetails
+                                UD.paymentImgURL = `${downloadURL}`
+                                RegInfo = [...RegInfo,UD]
+                                await updateDoc(doc(db,"events",Evpayment.notificationGroup),{  
+                                    "Registered Emails":RegEmails,
+                                    "Registered Users":RegInfo,
+                                }).then(async()=>{
+                                    await updateDoc(doc(db,"users",currentUser.uid),{
+                                        allRegisteredEvents:UserEvents
+                                    })
+                                    navigate("/RegisteredEvents")
+                                    alert(`Registered for ${Evpayment.name}`)
                                 })
-                                navigate("/RegisteredEvents")
-                                alert(`Registered for ${Evpayment.name}`)
-                            })
-                        }
-                        catch(err){
-                            setErr(true)
-                            console.log(err)
-                        }
+                            }
+                            catch(err){
+                                setErr(true)
+                                console.log(err)
+                            }
+                    })
                 })
-            })
+            }
+            else{
+                for(let i=0;i<details.length;i++){
+                    RegEmails = [...RegEmails,`${details[i].email}`]
+                }
+                // RegEmails = [...RegEmails,`${currentUser.email}`]
+
+                let RegInfo = temp[0]["Registered Teams"]
+                let UserEvents = []
+                // UserEvents = [...UserEvents,`${temp[0].notificationGroup}`]
+                await uploadBytesResumable(storageRef,PaymentSS)
+                    .then(()=>{
+                        getDownloadURL(storageRef).then(async (downloadURL) => {
+                            try{
+                                RegInfo = [...RegInfo,{teamName:teamName.teamName,teamCulture:teamName.Culture,teamDetails:details,paymentImgURL:`${downloadURL}`,paymentVerified:"N"}]
+                                console.log({teamName:teamName.teamName,teamCulture:teamName.Culture,teamDetails:details,paymentImgURL:`${downloadURL}`,paymentVerified:"N"})
+                                await updateDoc(doc(db,"events",Evpayment.notificationGroup),{  
+                                    "Registered Emails":RegEmails,
+                                    "Registered Teams":RegInfo,
+                                }).then(async()=>{
+                                    for(let i=0;i<4;i++){
+                                        console.log('hi')
+                                        const q = query(usersRef,where('email','==',details[i].email))
+                                        const temp1 = []
+                                        const querySnapShot = await getDocs(q)
+                                        console.log(querySnapShot)
+                                        console.log(temp1)
+                                        try{
+                                            querySnapShot.forEach((doc)=>{
+                                                temp1.push({ id: doc.id, ...doc.data() })
+                                            })
+                            
+                                            UserEvents = temp1[0].allRegisteredEvents
+                                        }catch(err){
+                                            console.log(err)
+                                        }
+                                        if(temp1.length>0){
+                                            UserEvents = [...UserEvents,`${temp[0].notificationGroup}`]
+                                            await updateDoc(doc(db,"users",temp1[0].id),{
+                                                allRegisteredEvents:UserEvents
+                                            })
+                                        }
+                                    }
+
+                                    navigate("/RegisteredEvents")
+                                    alert(`Registered for ${Evpayment.name}`)
+                                })
+                            }
+                            catch(err){
+                                setErr(true)
+                                console.log(err)
+                            }
+                    })
+                })
+            }
         }
     }
 
     return(
         <div className="Home payment-page">
         <div className="Login">
-            <div className="FormBox" style={{top:'10%'}}>
-                <form onSubmit={(e)=>HandleSubmit(e)}>
-                        <img src='https://firebasestorage.googleapis.com/v0/b/android-club-65a70.appspot.com/o/randomqrcode.jpeg?alt=media&token=38a7db21-144a-4684-8a9b-a9dbf6881f4d' width='30%' style={{alignSelf:'center'}}></img>
-                        <label htmlFor="Fl"><img src={ProfilePicIcon} style={{height:'50px',alignSelf:'center'}}></img><p style={{marginLeft:'5%',marginTop:"3%"}}>Add Payment Screenshot</p></label>
-                        <input id="Fl" type="file" placeholder="file" style={{display:'none'}} required></input>
-                        <input type="submit" id="S" value="Register"></input>
-                        {err && <span style={{alignSelf:'center'}}>Something went wrong, Try Again</span>}
-                        <p style={{width:'26%',color:'black'}}>Have an Account? <b><Link style={{textDecoration:'none'}} to="/login">Login Now</Link></b></p>
+        {!details[0] && Evpayment.noOfMembers>1 && 
+            <div className="FormBox">
+                <form onSubmit={(e)=>HandleDetailsSubmit(e)}>
+                    <label>Team Name</label>
+                    <input type='text' placeholder="Team-Name"></input>
+                    <label>Mention the culture that you'll be working with. (Read Instructions before choosing!!.) </label>
+                    <input type='text' placeholder='Culture -'></input>
+                    <div className="detailsSet">
+                        <label>Details of Team leader member</label>
+                        <input type='text' placeholder="name"></input>
+                        <input type='number' placeholder="phone-number of Team leader"></input>
+                        <input type='text' placeholder="Registration Number of Team leader" required></input>
+                        <input type='email' placeholder="College Email of Team leader" required></input>
+                    </div>
+
+                    <div className="detailsSet">
+                        <label>Details of 2nd member</label>
+                        <input type='text' placeholder="Registration Number of 2nd member" required></input>
+                        <input type='email' placeholder="College Email of 2nd member" required></input>
+                    </div>
+
+                    <div className="detailsSet">
+                        <label>Details of 3rd member</label>
+                        <input type='text' placeholder="Registration Number of 3rd member"></input>
+                        <input type='email' placeholder="College Email of 3rd member"></input>
+                    </div>
+
+                    <div className="detailsSet">
+                        <label>Details of 4th member</label>
+                        <input type='text' placeholder="Registration Number of 4th member"></input>
+                        <input type='email' placeholder="College Email of 4th member"></input>
+                    </div>
+                    <input type='submit' value='Next'></input>
+
                 </form>
             </div>
+
+        }
+        {(details[0] || Evpayment.noOfMembers==1) &&            
+            <div className="FormBox" style={{top:'10%'}}>
+                <form onSubmit={(e)=>HandleSubmit(e)}>
+                    <img src='https://firebasestorage.googleapis.com/v0/b/android-club-65a70.appspot.com/o/randomqrcode.jpeg?alt=media&token=38a7db21-144a-4684-8a9b-a9dbf6881f4d' width='30%' style={{alignSelf:'center'}}></img>
+                    <p>OR</p>
+                    <p style={{textAlign:'center'}}>Clubs bank account details: <br></br>
+                        Account Number: 6565521552 <br></br>
+                        IFSC Code: IDIB000V143 <br></br>
+                        Bank Name: Indian Bank, VIT Bhopal University,  Kothrikalan 
+                    </p>
+                    {
+                        UserDetails.regNo[1] == 1 &&
+                        <label>Event fees - ₹300</label>  
+                    }  
+                    {
+                        UserDetails.regNo[1] == 2 &&
+                        <label>Event fees - ₹100</label> 
+                    }                
+                    <label htmlFor="Fl"><img src={ProfilePicIcon} style={{height:'50px',alignSelf:'center'}}></img><p style={{marginLeft:'5%',marginTop:"3%"}}>Add Payment Screenshot</p></label>
+                    <input id="Fl" type="file" placeholder="file" style={{display:'none'}} required></input>
+                    <input type="submit" id="S" value="Register"></input>
+                    {err && <span style={{alignSelf:'center'}}>Something went wrong, Try Again</span>}
+                    <p style={{width:'26%',color:'black'}}>Have an Account? <b><Link style={{textDecoration:'none'}} to="/login">Login Now</Link></b></p>
+                </form>
+            </div>
+        }
         </div>
     </div>
     )
