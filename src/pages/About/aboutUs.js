b07b52d67659215576f6af3b338303a829f77901
dewansh07 <dewanshwadhwa@gmail.com>
2023-07-17 15:57:37 +0530

    updated:about us

--- src/pages/About/aboutUs.js
+++ src/pages/About/aboutUs.js
@@ -1,37 +1,31 @@
 import Navbar from "../../components/Navbar/Navbar"
 import { useEffect, useState } from "react";
-import { collection, query } from "firebase/firestore";
+import { collection, doc, getDocs, getDoc } from "firebase/firestore";
 import { db } from "../../firebaseconfig";
-import { getDocs } from "firebase/firestore";
 import "./about.css"
 import { Footer } from "../../components/Footer/Footer";
 
 function AboutUs() {
-    const [team, setTeam] = useState([])
+    const [teamMembers, setTeamMembers] = useState([])
 
-    const usersRef = collection(db, "team");
-    const FetchEvents = async () => {
-        const q = query(usersRef)
-        const temp = []
-        const querySnapShot = await getDocs(q)
-        try {
-            querySnapShot.forEach((doc) => {
-                temp.push(doc.data())
-            })
-            setTeam(temp)
-        } catch (err) {
-            console.log(err)
-        }
-    }
     useEffect(() => {
-        FetchEvents()
-        // eslint-disable-next-line
-    }, [])
+        const fetchData = async () => {
+          const teamMembersRef = doc(db, "Our Team", "ADMIN", "President", "1");
+          const teamMembersSnapshot = await getDoc(teamMembersRef);
+          if (teamMembersSnapshot.exists()) {
+            setTeamMembers(teamMembersSnapshot.data());
+          }
+        };
+    
+        fetchData();
+      }, []);
+    
 
     return (
         <div className="About">
             <Navbar className="navbar-about" />
             <div className="about-club">
+                <br />
                 <h1>Welcome to <br /> Android club VIT Bhopalll </h1> <br />
                 {/* <h1>Android club VIT Bhopal </h1> <br /> */}
                 <p>
@@ -55,39 +49,30 @@ function AboutUs() {
 
                     <strong>Leadership:</strong><br />
                     Our club is led by a team of passionate and dedicated students who are committed to promoting the learning and development of Android app development among our members. Our leadership team includes: <br /> <br />
-{/* 
+
                     President: [Name] <br />
                     Vice-President: [Name] <br />
-                    Treasurer: [Name] <br /> <br /> */}
+                    Treasurer: [Name] <br /> <br />
                     <strong>Call to action:</strong> <br />
                     If you are interested in joining our club or have any questions, please don't hesitate to get in touch with us. You can reach us at [Email Address] or [Social Media Handles]. We look forward to hearing from you!
                 </p>
             </div>
-                {/* <h1 className="meet-team-heading">Meet the Team</h1>
-            <div className="teams">
-                {
-                    team.map((data) => (
-                        <div key={Math.random()} className="team">
-                            <div className="team-member-info" >
-                                <p className="photo"><b></b> <img src={data.photo} alt="" className="team-member-img" /> </p>
-                                <p className="team-member-name">{data.name}</p>
-                                <p className="team-member-desc">{data.description}</p>
-                                <p className="team-member-pos"><b></b>{data.position}</p>
-                                <a href="https://www.linkedin.com" target="_blank">
-                                    <i className=" linkedin icon fa-brands fa-linkedin"></i>
-                                </a>
-                                <a href="https://www.instagram.com/" target="_blank">
-                                    <i className=" insta icon fa-brands fa-instagram"></i>
-                                </a>
-                                <a href="https://www.github.com/" target="_blank">
-                                    <i className=" github icon fa-brands fa-github"></i>
-                                </a>
-                                
-                            </div>
-                        </div>
-                    ))
-                }
-            </div> */}
+                <h1 className="meet-team-heading">Meet the Team</h1>
+                <br />
+            <div className="blue-container1">
+                <h2>President</h2>
+                <p>{teamMembers.name}</p>
+                <p>{teamMembers.email}</p>
+
+            </div>
+                <br />
+                <div className="blue-container2"></div>
+                <div className="blue-container3"></div>
+                <div className="blue-container4"></div>
+                <div className="blue-container5"></div>
+                <div className="blue-container6"></div>
+                <div className="blue-container7"></div>
+
             <Footer/>
 
         </div>
